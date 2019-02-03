841e2811ea34f8d2e5ba7ae0397f3d617884cca0
Edwardzcn <edwardzcn98@gmail.com>
2019-02-03 19:58:20 +0800

    Site updated: 2019-02-03 19:58:20

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [];
+var precacheConfig = [["/sw-register.js","3ffb7e6b52e4eaf3a677ed810b5219f8"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
